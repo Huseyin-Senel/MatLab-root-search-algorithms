d953fa52201c4f92be6fa535f13a8a8f934fb17b
Huseyin-Senel <96916049+Huseyin-Senel@users.noreply.github.com>
2022-01-01 15:48:59 +0300

    Add files via upload

---
+++ The Secant Method/The_Secant.m
@@ -0,0 +1,67 @@
+clear;
+clc;
+close all;
+
+
+f = @(x) (x.^2)-(5*x)-10;
+
+x0 = input('Başlangıç değerini girin: ');
+x1 = input('Bitiş değerini girin: ');
+
+N = input('Yineleme değerini girin: ');
+es = input('Hata oranını girin: ');
+
+x=x0:0.1:x1;
+y=f(x);
+figure(1),
+plot(x,y)
+xlabel('X Values')
+ylabel('f(x)')
+grid on;
+
+
+if(f(x0)*f(x1)>0)
+    disp('Hata: kök x1 ile x2 arasında değil');
+    return;
+end
+
+
+i=0;
+while(i<=N)
+    i=i+1;
+    
+    x2 = x1-(f(x1)*((x1-x0)/(f(x1)-f(x0))));
+    
+    %Hata
+    ea =abs(((x2-x1)/x2)*100);
+    
+    if(f(x2)==0)
+        msg = sprintf('Deneme = %d, Kök = %f, Hata = %f',i,x2,ea);
+        disp(msg);
+        figure(1);
+        plot(x,y,x2,f(x2),'o');
+        title(msg);
+        xlabel('X Values')
+        ylabel('f(x)')
+        grid on;
+        break; %kök bulundu
+    end 
+    
+    msg = sprintf('Deneme = %d, Kök = %f, Hata = %f',i,x2,ea);
+    disp(msg);
+    figure(1);
+    plot(x,y,x2,f(x2),'o');
+    title(msg);
+    xlabel('X Values')
+    ylabel('f(x)')
+    grid on;
+    pause(0.7);
+    
+    % durdu
+    if(ea<es)
+        break;
+    end
+    
+    x0 = x1;
+    x1 = x2;
+end
